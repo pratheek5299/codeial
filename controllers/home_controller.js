d606b9ec3031e72d26a851152b16993efe11ef40
Pratheek <saipratheekreddykasarla@gmail.com>
2023-10-27 12:05:32 +0530

    Add a controller and a router for home

---
+++ controllers/home_controller.js
@@ -0,0 +1,4 @@
+module.exports.home = function(request, response){
+    return response.end('<h1>Express is up for codeial </h1>');
+}
+//module.exports.action_name = function(request, response){}
--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express();
 //by default port number is 80
 const port = 8000;
 
+//use express router
+app.use('/', require('./routes'))
+
 app.listen(port, function(err){
     if(err){
         // console.log('Error: ', err);
