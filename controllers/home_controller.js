2698388cf4a6a3838a68b6e51461e51ad3d2669c
Pratheek <saipratheekreddykasarla@gmail.com>
2023-11-09 12:10:09 +0530

    Added comments section

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -11,7 +11,15 @@ module.exports.home = function(request, response){
     //     });
     // })
 
-    Post.find({}).populate('user').exec().then(function(allPosts){
+    Post.find({})
+    .populate('user')//ref to models/posts.js in the schema user
+    .populate({
+        path: 'comments',//ref to models/posts.js in the schema comments
+        populate: {
+            path: 'user'
+        }
+    })
+    .exec().then(function(allPosts){
         return response.render('home',{
             title: 'Codeial | Home',
             all_posts: allPosts
